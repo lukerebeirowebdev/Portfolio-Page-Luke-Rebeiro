eab4cf239528e6eac75710bf5cb3f6763d8b2eb1
Luke Rebeiro <lukerebeiro@hotmail.co.uk>
2020-08-25 15:40:22 +0100

    php changes

--- form.php
+++ form.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailto = "lukerebeiro@hotmail.com";
+    $mailto = "";
     $headers = "From; ".$mailFrom;
     $txt = "You have received an email from. " $name.".\n\n".$message;
 
