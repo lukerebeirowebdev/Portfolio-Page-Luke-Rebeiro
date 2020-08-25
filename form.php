cf1ecd11063a950db8f3ba30b70421b6b6fc88be
Luke Rebeiro <lukerebeiro@hotmail.co.uk>
2020-08-25 15:27:29 +0100

    changes

---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['fullname'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailto = "lukerebeiro@hotmail.com";
+    $headers = "From; ".$mailFrom;
+    $txt = "You have received an email from. " $name.".\n\n".$message;
+
+    mail($mailto, $subject, $txt, $headers);
+
+    header("Location: index.php?mailsend");
+}
